a4b5ed44d5b7fa0e5d34917bd00c6810104def89
DopMephy <alexander.pernkopf@dopplerschirme.com>
2016-12-14 11:11:21 +0100

    Changed biblio scripts, loading from hashilb directly

--- crack.py
+++ crack.py
@@ -2,41 +2,38 @@
 
 import getopt
 import sys
-
-
-from biblio.crack_md5 import *
-
 from identify import *
 
 
 def get_mode():
-	print("test")
+    print("test")
+
 
 def get_input():
-	usage = "To identifiy the hash use 'python crack.py -i <hashvalue>'.\nTo crack the hash use 'python crack.py -c <hashvalue> <wordlist-file>'"
+    usage = "To identifiy the hash use 'python crack.py -i <hashvalue>'.\nTo crack the hash use 'python crack.py -c <hashvalue> <wordlist-file>'"
 
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], 'c:i:m:h', ['crack=', 'identify=', 'mode=', 'help'])
-		test = opts
-	except getopt.GetoptError:
-		print(usage)
-		sys.exit(2)
-	flag = 0
-	helper = opts
-	for opt, arg in opts:
-		if opt in ('-h', '--help'):
-			print(usage)
-		elif opt in ('-i', '--identify'):
-			identifiy(sys.argv[2])
-		elif (opt in ('-c', '--crack')):
-			for blubb in test:
-				if blubb in ('-m', '--mode'):
-					get_mode()
-					sys.exit(0)
-		else:
-			print(usage)
-			sys.exit(2)
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'c:i:m:h', ['crack=', 'identify=', 'mode=', 'help'])
+        test = opts
+    except getopt.GetoptError:
+        print(usage)
+        sys.exit(2)
+    flag = 0
+    helper = opts
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print(usage)
+        elif opt in ('-i', '--identify'):
+            identifiy(sys.argv[2])
+        elif (opt in ('-c', '--crack')):
+            for blubb in test:
+                if blubb in ('-m', '--mode'):
+                    get_mode()
+                    sys.exit(0)
+        else:
+            print(usage)
+            sys.exit(2)
 
 
 if __name__ == "__main__":
-	get_input()
+    get_input()
--- identify.py
+++ identify.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 import os
 import inspect
-import importlib.util
+from time import *
 
-
+#User I/O
 def choice(default="y"):
     if (default == "y"):
         yes = ['yes', 'y', 'ye', '']
@@ -20,9 +20,43 @@ def choice(default="y"):
     else:
         print("Please respond with 'yes' or 'no'")
 
-
+#Returns location of itself
 def _getLocation():
     return os.path.dirname(inspect.stack()[0][1])
+
+#loads dynamicly Hashtyps from hashlib
+def cracker(hashTyp, hash, wordlist):
+
+    moduleLoader = __import__('hashlib', fromlist=[hashTyp])
+    hashClass = getattr(moduleLoader, hashTyp)
+
+
+    print("\n---------------------Parameters ---------------------")
+    print('Hash:', hash)
+    print('Wordlistpath:', wordlist)
+    print("\n---------------------Start cracker---------------------")
+
+    counter = 0
+    clockStart = clock()
+    with open(wordlist,"r") as f:
+        for line in f:
+            counter += 1
+
+            if counter % 500000 == 0:
+                print(counter, "Hashes calculated")
+
+            calc = hashClass(line.encode()).hexdigest()
+
+            if hash == calc:
+                print("\n============Result================\n\nYour Password is", line)
+                break
+        else:
+            print("\nNo Hash found! Maybe try another wordlist.")
+    clockEnd = clock()
+
+    runTime = clockEnd - clockStart
+    print("\nHad to try {0} Hashes.\nCalculated {1:.4f} Seconds.".format(counter,runTime))
+
 
 
 def _callCracker(hash, hashTyp):
@@ -30,29 +64,23 @@ def _callCracker(hash, hashTyp):
     if (choice()):
         print("Do you want to use the default wordlist? (Y/n)")
 
-        spec = importlib.util.spec_from_file_location("module.name", os.path.join(_getLocation(),"biblio","crack_{0}.py".format(hashTyp)))
-        loadmodul = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(loadmodul)
-
         if (choice()):
-            loadmodul.Cracker(hash, os.path.join(_getLocation(), "wordlist", "default.txt"))
+            cracker(hashTyp, hash, os.path.join(_getLocation(), "wordlist", "default.txt"))
         else:
             print("Please insert your wordlist:")
-            loadmodul.Cracker(hash, input())
+            cracker(hashTyp, hash, input())
 
-
+#idetifies with the lenght of the hash which typ it is
+#TODO: this can be done better
 def identifiy(hash):
     print("\n---------------------Parameters ---------------------")
     print('Hash:', hash)
     print("\n---------------------Identifier---------------------")
-    found = 0
-    if len(hash) == 32:
-        found = 1
-        _callCracker(hash,"md5")
+#whirelpool:128,...
+    hashBiblio={32:"md5",40:"sha",64:"sha256",128:"sha512"}
 
-    if len(hash) == 40:
-        found = 1
-        _callCracker(hash,"sha1")
-
-    if (not (found)):
+    try:
+        _callCracker(hash,hashBiblio[len(hash)])
+    except:
         print("Unknown hash")
+
